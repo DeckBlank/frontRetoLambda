870e86425d6b150251efaf87322bcd5a93fdbbbb
Jose Luciano Guzman Santiago <deckblank@gmail.com>
2023-10-02 18:03:46 -0500

    fix js to ts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <h2>Ingresa tu Tarjeta de Crédito (este es un formulario de prueba no ingresar datos reales)</h2>
+    <h2>
+      Ingresa tu Tarjeta de Crédito (este es un formulario de prueba no ingresar
+      datos reales)
+    </h2>
     <form @submit.prevent="submitForm">
       <div class="form-group">
         <label for="card_number">Número de Tarjeta:</label>
-        <input type="number" id="card_number" v-model="formData.card_number" required />
+        <input
+          type="number"
+          id="card_number"
+          v-model="formData.card_number"
+          required
+        />
       </div>
       <div class="form-group">
         <label for="email">Correo Electrónico:</label>
@@ -16,11 +24,21 @@
       </div>
       <div class="form-group">
         <label for="expiration_year">Año de Expiración:</label>
-        <input type="text" id="expiration_year" v-model="formData.expiration_year" required />
+        <input
+          type="text"
+          id="expiration_year"
+          v-model="formData.expiration_year"
+          required
+        />
       </div>
       <div class="form-group">
         <label for="expiration_month">Mes de Expiración:</label>
-        <input type="text" id="expiration_month" v-model="formData.expiration_month" required />
+        <input
+          type="text"
+          id="expiration_month"
+          v-model="formData.expiration_month"
+          required
+        />
       </div>
       <button type="submit">Enviar</button>
     </form>
@@ -28,48 +46,59 @@
 </template>
 
 <script lang="ts">
-import axios from 'axios'
-import { VUE_APP_API_TOKEN, VUE_APP_TOKEN } from '../env'
-export default {
+interface OptionalData {
+  card_number : number
+email: string
+cvv : number
+expiration_year : string
+expiration_month : string
+}
+import axios from "axios";
+import { defineComponent } from "vue"
+import { VUE_APP_API_TOKEN, VUE_APP_TOKEN } from "../env";
+export default defineComponent({
   data() {
     return {
       formData: {
-    card_number : 4532015112830366,
-    email :"deckblqnk@gmail.com",
-    cvv : 101,
-    expiration_year :"2024",
-    expiration_month :"12"
-}
-    }
+        card_number: 4532015112830366,
+        email: "deckblqnk@gmail.com",
+        cvv: 101,
+        expiration_year: "2024",
+        expiration_month: "12",
+      } as OptionalData,
+    };
   },
   methods: {
-    submitForm(event) {
-      // Evitar que el formulario se envíe de forma predeterminada.
-      event.preventDefault()
-      console.log(event)
-
-      // Aquí puedes realizar la lógica que necesites con los datos del formulario.
-      console.log('Nombre:', this.formData)
-
-      // Puedes enviar los datos al servidor o realizar cualquier otra acción aquí.
-      // enviamos los datos a api
-      // y agregamos el header authorization con el token
-      axios.post(VUE_APP_TOKEN, this.formData, {
-        headers: {
-          Authorization: `Bearer ${VUE_APP_API_TOKEN}`
-        }
-      })
-        .then(response => {
-          console.log(response)
+    submitForm(event: any) {
+      event.preventDefault();
+      const {
+        card_number,
+        email,
+        cvv,
+        expiration_year,
+        expiration_month,
+      } = this.formData;
+      axios
+        .post(VUE_APP_TOKEN,{
+        card_number,
+        email,
+        cvv,
+        expiration_year,
+        expiration_month,
+      } , {
+          headers: {
+            Authorization: `Bearer ${VUE_APP_API_TOKEN}`,
+          },
         })
-        .catch(error => {
-          console.log(error)
+        .then((response) => {
+          console.log(response);
         })
-
-      
-    }
-  }
-}
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+});
 </script>
 
 <style scoped>
@@ -81,9 +110,9 @@ label {
   font-weight: bold;
 }
 
-input[type='text'],
-input[type='number'],
-input[type='email'] {
+input[type="text"],
+input[type="number"],
+input[type="email"] {
   width: 100%;
   padding: 8px;
   border: 1px solid #ccc;
